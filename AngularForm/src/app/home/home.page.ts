e23adc093a50f459fc7c3ba11a4578b595d49c2d
fabeto10 <f0414475@gmail.com>
2022-10-24 22:03:43 -0400

    se crea get y se hace funcionar, falta implementar search y post

--- AngularForm/src/app/home/home.page.ts
+++ AngularForm/src/app/home/home.page.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 
+import { DatosService } from '../../app/datos.service'
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  datos: any = {};
+  postData: [];
+  constructor(private datosService: DatosService ) {
+    this.datosService.getDesarrollador().subscribe(data => {
+      this.datos = data;
+    })
+  }
+  ngOnInit(){
+    // this.http.post<any>('https://alternos.sgc-consultores.com.ve/pruebatour/registro', {title: 'Request'}).subscribe(data => {
+    //   this.postData = data.id
+    // })
+    this.datosService.getDesarrollador().subscribe(data => {
+      this.datos = data;
+    })
+  }
+  public viewDesarrollador(){
+    this.datosService.getDesarrollador().subscribe(data => {
+      this.datos = data;
+      console.log(this.datos);
+    })
+  }
 }
